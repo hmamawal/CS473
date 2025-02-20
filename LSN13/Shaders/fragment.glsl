14c1c5b1c2aa0de802c09fe384f3ec5b3b0b954e
Hunter <113301722+hmamawal@users.noreply.github.com>
2025-02-20 09:14:11 -0500

    step 2 working (i think)

--- LSN13/Shaders/fragment.glsl
+++ LSN13/Shaders/fragment.glsl
@@ -7,28 +7,45 @@ in vec3 normal;
 in vec3 fragment_position;
 
 uniform vec4 set_color;
-
 uniform sampler2D our_texture;
 
-uniform vec4 light_color;
-uniform vec4 light_position;
+struct PointLight {
+    vec4 position;
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+};
+
+uniform PointLight point_light;
 uniform vec4 view_position;
 uniform float ambient_strength;
 
+vec4 CalcPointLight(PointLight light, vec3 frag_pos, vec3 normal, vec3 view_dir);
+
 void main()
 {
-    vec3 light_direction = normalize(light_position.xyz - fragment_position);
-    vec3 view_direction = normalize(view_position.xyz - fragment_position);
-    
     vec3 n = normalize(normal);
-    vec3 reflect_direction = reflect(-light_direction,n);
-    float diff_strength = max(dot(n,light_direction),0.0);
-    float spec_strength = pow(max(dot(view_direction,reflect_direction),0.0),256);
+    vec3 view_dir = normalize(view_position.xyz - fragment_position);
+    vec4 point_light_color = CalcPointLight(point_light, fragment_position, n, view_dir);
+
     if (textured > 0) {
-        FragColor = texture(our_texture, TexCoord);
+        FragColor = texture(our_texture, TexCoord) * point_light_color;
     } else {
-        FragColor = set_color;
+        FragColor = set_color * point_light_color;
     }
-    FragColor = (spec_strength + diff_strength + ambient_strength) * light_color * FragColor;
-    
+}
+
+vec4 CalcPointLight(PointLight light, vec3 frag_pos, vec3 normal, vec3 view_dir)
+{
+    vec3 light_dir = normalize(light.position.xyz - frag_pos);
+    vec3 reflect_dir = reflect(-light_dir, normal);
+
+    float diff_strength = max(dot(normal, light_dir), 0.0);
+    float spec_strength = pow(max(dot(view_dir, reflect_dir), 0.0), 256);
+
+    vec4 ambient = light.ambient * ambient_strength;
+    vec4 diffuse = light.diffuse * diff_strength;
+    vec4 specular = light.specular * spec_strength;
+
+    return ambient + diffuse + specular;
 }
--- LSN13/main.cpp
+++ LSN13/main.cpp
@@ -8,16 +8,14 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-
-
-//Processes user input (keyboard currently).
-//Receives a GLFWwindow pointer as input.
+// Processes user input (keyboard currently).
+// Receives a GLFWwindow pointer as input.
 void processInput(GLFWwindow *window);
 
-//Processes mouse movement
+// Processes mouse movement
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
-//Global Variables
+// Global Variables
 glm::vec4 clear_color(0.0,0.0,0.0,1.0);
 glm::vec4 outline_color = glm::vec4(0.0,0.0,0.0,1.0);
 
@@ -30,14 +28,13 @@ Camera camera(glm::vec3(0.0,0.0,3.0),glm::vec3(0.0,1.0,0.0),-90.0,0.0f);
 float delta_time = 0.0;
 float last_frame = 0.0;
 
-//mouse movement variables
+// Mouse movement variables
 float last_x = 400;
 float last_y = 400;
 bool first_mouse = true;
 float pitch = 0.0;
 float yaw = -90.0;
 glm::vec3 camera_front = glm::vec3(0.0,0.0,-1.0);
-
 
 int main () {
     GLFWwindow *window = InitializeEnvironment("ICE 11.1",800,800);
@@ -47,15 +44,14 @@ int main () {
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
     glfwSetCursorPosCallback(window, mouse_callback); 
     
-    //create shader program
+    // Create shader program
     Shader shader("./Shaders/vertex.glsl","./Shaders/fragment.glsl");
     
-    //get a texture from memory
+    // Get textures from memory
     unsigned int tile_texture = GetTexture("./images/tile.jpg");
     unsigned int wood_texture = GetTexture("./images/wood.jpg");
 
-
-    //1. define your Vertex Array Object, used to hold attribute pointers
+    // Define Vertex Array Objects
     VAOStruct position_vao;
     glGenVertexArrays(1,&(position_vao.id));
     AttributePointer position_attr = BuildAttribute(3,GL_FLOAT,false,3*sizeof(float),0);
@@ -69,8 +65,8 @@ int main () {
     texture_vao.attributes.push_back(position_attr2);
     texture_vao.attributes.push_back(normal_attr);
     texture_vao.attributes.push_back(texture_attr);
- 
 
+    // Create shapes
     BasicShape bottom = GetTexturedRectangle(texture_vao,glm::vec3(-0.25,-0.25,0.0),0.5,0.5,1.0,true);
     BasicShape front_side = GetTexturedTriangle(texture_vao,glm::vec3(-0.25, -0.25, 0.0),
                                                            glm::vec3(0.25, -0.25, 0.0),
@@ -95,9 +91,7 @@ int main () {
         glm::vec3(0.0,-1.0,0.0),
         glm::vec3(-1.0,0.0,0.0),
         glm::vec3(0.0,1.0,0.0),
-
     };
-
 
     glm::mat4 identity = glm::mat4(1.0f);
     glm::mat4 local = identity;
@@ -107,21 +101,23 @@ int main () {
 
     view = glm::translate(view,glm::vec3(0.0,0.0,-4.0));
     projection = glm::perspective(glm::radians(45.0f),1.0f,0.1f,100.0f);
-    
 
     glEnable(GL_DEPTH_TEST);
-    //The render loop -- the function in the condition checks if the 
-    //  window has been set to close (does this each iteration)
-    while (!glfwWindowShouldClose(window)) {
 
-        //calculate the delta time
+    // Set point light properties
+    shader.setVec4("point_light.position", glm::vec4(0.0, 10.0, 0.0, 1.0));
+    shader.setVec4("point_light.ambient", glm::vec4(0.1, 0.1, 0.1, 1.0));
+    shader.setVec4("point_light.diffuse", glm::vec4(1.0, 1.0, 1.0, 1.0));
+    shader.setVec4("point_light.specular", glm::vec4(1.0, 1.0, 1.0, 1.0));
+
+    // The render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Calculate the delta time
         float current_frame = glfwGetTime();
         delta_time = current_frame - last_frame;
         last_frame = current_frame;
-        
 
-
-        //input
+        // Input
         processInput(window);
         
         view = camera.GetViewMatrix();
@@ -130,32 +126,21 @@ int main () {
         shader.setMat4("view",view);
         shader.setMat4("projection",projection);
         shader.setFloat("ambient_strength",0.1);
-        shader.setVec4("light_color",glm::vec4(1.0,1.0,1.0,1.0));
-        shader.setVec4("light_position",glm::vec4(0.0,10.0,0.0,1.0));
         shader.setVec4("view_position",glm::vec4(camera.Position,1.0));
 
-
-        //render commands here
-        //set the clear color to wipe the window
+        // Render commands
         glClearColor(clear_color.r,clear_color.g,clear_color.b,clear_color.a);
-
-
-        //clear the color buffer (where things are drawn) using the current clear color.
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         for (int j = 0; j < 10; j++) {
-        
             for (int i = 0; i < pyramid_locations.size(); i++) {
-                //set the model matrix to the identity matrix
-                
                 local = glm::rotate(identity,glm::radians(angle_x),glm::vec3(1.0,0.0,0.0));
                 local = glm::rotate(local,glm::radians(angle_z),glm::vec3(0.0,0.0,1.0));
                 shader.setMat4("local",local);
-                //model = glm::rotate(identity,glm::radians(-45.0f),glm::vec3(1.0,0.0,0.0));
                 model = glm::translate(identity,pyramid_locations[i]+glm::vec3(0.0,0.0,-2.0*j));
                 shader.setMat4("model",model);
 
-                //render the shapes here
+                // Render the shapes
                 glBindTexture(GL_TEXTURE_2D,tile_texture);
                 shader.setBool("is_textured",true);
                 bottom.Draw();
@@ -164,28 +149,17 @@ int main () {
                 right_side.Draw();
                 left_side.Draw();
                 front_side.Draw();
-
             }
         }
 
-        
-
-
-        //check and call events and swap the buffers
-
-        //Applications are usually drawn in a double buffer...one for drawing one for displaying
-        //you draw to one and then you swap the buffers...avoids flickering.
+        // Swap buffers and poll events
         glfwSwapBuffers(window);
-
-        //checks if any events are triggered (keyboard or mouse input)
         glfwPollEvents();
     }
 
-    //terminate your window when you leave the render loop
+    // Terminate the window
     glfwTerminate();
-    //must have a return value 0 indicates successful run...in this case yes.
     return 0;
-
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -209,24 +183,14 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     yaw   += xoffset;
     pitch += yoffset;
     camera.ProcessMouseMovement(xoffset,yoffset);
-    // if(pitch > 89.0f)
-    //     pitch = 89.0f;
-    // if(pitch < -89.0f)
-    //     pitch = -89.0f;
-
-    // glm::vec3 direction;
-    // direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    // direction.y = sin(glm::radians(pitch));
-    // direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    // camera_front = glm::normalize(direction);
-}  
+}
 
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    //Color Key Processing
+    // Color Key Processing
     int keys[] {GLFW_KEY_R,GLFW_KEY_G,GLFW_KEY_B};
     for (int i = 0; i < 3; i++) {
         if ((glfwGetKey(window,keys[i])==GLFW_PRESS) && (!clear_key_pressed[i])){
@@ -242,7 +206,7 @@ void processInput(GLFWwindow *window)
         }
     }
 
-    //rotation key processing
+    // Rotation key processing
     if (glfwGetKey(window,GLFW_KEY_LEFT) == GLFW_PRESS) {
         angle_z += 2.0;
     }
@@ -256,7 +220,7 @@ void processInput(GLFWwindow *window)
         angle_x += 2.0;
     }
 
-    //camera key processing
+    // Camera key processing
     if (glfwGetKey(window,GLFW_KEY_W) == GLFW_PRESS) {
         camera.ProcessKeyboard(FORWARD,delta_time);
     }
@@ -269,9 +233,6 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window,GLFW_KEY_D) == GLFW_PRESS) {
         camera.ProcessKeyboard(RIGHT,delta_time);
     }
-
-
-
 }
 
 
